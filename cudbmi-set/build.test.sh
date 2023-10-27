73c2b1acc356e12c2850a568546e32a38c107eff
d33bs <dave.bunten@cuanschutz.edu>
2023-10-27 16:05:57 -0600

    change build test to ensure proper exit

--- cudbmi-set/build.test.sh
+++ cudbmi-set/build.test.sh
@@ -5,11 +5,20 @@
 # setting to exit the script on any failures
 set -e
 
-# switch user to ubuntu
-su - ubuntu
+# switch user to ubuntu and run commands
+su - ubuntu <<'EOF'
+    # cd to the home dir for ubuntu user
+    cd /home/ubuntu
 
-# cd to the home dir for ubuntu user
-cd /home/ubuntu
+    # run the build script
+    bash -x /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+EOF
 
-# run the build script
-bash -x /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+# seek success text in specific log file
+if grep -q "======= script finished ======" "/home/ubuntu/kg2-build/setup-kg2-build.log"; then
+    echo "setup-kg2-build.sh finished successfully."
+else
+    echo "Error: setup-kg2-build.sh did not finish successfully."
+    exit 1
+fi
+
