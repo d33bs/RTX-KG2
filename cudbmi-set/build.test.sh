123a450daf42e9ff15138d3bac620b107db48251
d33bs <dave.bunten@cuanschutz.edu>
2023-11-06 14:03:19 -0700

    append build requirements to attempt fix

--- cudbmi-set/build.test.sh
+++ cudbmi-set/build.test.sh
@@ -8,6 +8,11 @@ set -e
 # cd to the home dir for ubuntu user
 cd /home/ubuntu
 
+# prepend python dependencies for amending potential challenges
+(cat /home/ubuntu/RTX-KG2/cudbmi-set/requirements-build-append.txt \
+    && cat /home/ubuntu/RTX-KG2/requirements-kg2-build.txt) > temp.txt \
+    && sudo mv temp.txt /home/ubuntu/RTX-KG2/requirements-kg2-build.txt
+
 # run the build script, continuing even if there are errors
 sudo bash -x /home/ubuntu/RTX-KG2/setup-kg2-build.sh || true
 
