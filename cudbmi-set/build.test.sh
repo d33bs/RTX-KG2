6d265a91f2ff585ffbd8dbafc33a7091594fd00b
d33bs <dave.bunten@cuanschutz.edu>
2023-11-08 08:55:59 -0700

    pydantic workarounds; aws config; postgres fix

--- cudbmi-set/build.test.sh
+++ cudbmi-set/build.test.sh
@@ -10,7 +10,14 @@ cd /home/ubuntu
 
 # insert fixes into line 102 of /home/ubuntu/RTX-KG2/setup-kg2-build.sh 
 # for correcting python build requirements
-sudo sed -i.bak '102r /home/ubuntu/RTX-KG2/cudbmi-set/python-pydantic-core-workarounds.sh' /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+sudo sed -i.bak \
+    '102r /home/ubuntu/RTX-KG2/cudbmi-set/python-pydantic-core-workarounds.sh' \
+    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+
+# replace faulty postgres apt link in setup file (http vs https)
+# see: https://www.postgresql.org/download/linux/ubuntu/
+sed -i 's|http://apt.postgresql.org/pub/repos/apt|https://apt.postgresql.org/pub/repos/apt|g' \
+    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
 
 # prepend python dependencies for amending potential challenges
 (cat /home/ubuntu/RTX-KG2/cudbmi-set/requirements-build-append.txt \
