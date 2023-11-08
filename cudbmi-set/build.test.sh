f5c8c045bf4561ff36bdde77378f673dfdfaa4d6
d33bs <dave.bunten@cuanschutz.edu>
2023-11-08 15:17:35 -0700

    postgres and aws s3 reconfiguration to avoid error

--- cudbmi-set/build.test.sh
+++ cudbmi-set/build.test.sh
@@ -14,15 +14,22 @@ sudo sed -i.bak \
     '102r /home/ubuntu/RTX-KG2/cudbmi-set/python-pydantic-core-workarounds.sh' \
     /home/ubuntu/RTX-KG2/setup-kg2-build.sh
 
-# replace faulty postgres apt link in setup file (http vs https)
-# see: https://www.postgresql.org/download/linux/ubuntu/
-sed -i 's|http://apt.postgresql.org/pub/repos/apt|https://apt.postgresql.org/pub/repos/apt|g' \
-    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
-
 # prepend python dependencies for amending potential challenges
 (cat /home/ubuntu/RTX-KG2/cudbmi-set/requirements-build-append.txt \
     && cat /home/ubuntu/RTX-KG2/requirements-kg2-build.txt) > temp.txt \
     && sudo mv temp.txt /home/ubuntu/RTX-KG2/requirements-kg2-build.txt
+
+# redefine s3_cp_cmd after it is set to avoid non-AWS centric work
+sudo sed -i.bak '11s/.*/s3_cp_cmd="echo"/' /home/ubuntu/RTX-KG2/master-config.shinc
+
+# replace archived postgres apt link in setup file
+# see: https://www.postgresql.org/message-id/ZN4OigxPJA236qlg%40msg.df7cb.de
+sudo sed -i.bak \
+    's|http://apt.postgresql.org/pub/repos/apt|https://apt-archive.postgresql.org/pub/repos/apt|g' \
+    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+
+# start postgresql service in order to run subsequent commands
+sudo sed -i.bak '174s/.*/&\n    sudo service postgresql start/' /home/ubuntu/RTX-KG2/setup-kg2-build.sh
 
 # run the build script, continuing even if there are errors
 sudo bash -x /home/ubuntu/RTX-KG2/setup-kg2-build.sh || true
