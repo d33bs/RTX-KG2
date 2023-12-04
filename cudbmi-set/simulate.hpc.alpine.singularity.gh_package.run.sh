4ff1ec67ac98f84f70d47caff402b06cfcb70adb
d33bs <dave.bunten@cuanschutz.edu>
2023-12-04 15:16:55 -0700

    singularity run cmd

--- cudbmi-set/simulate.hpc.alpine.singularity.gh_package.run.sh
+++ cudbmi-set/simulate.hpc.alpine.singularity.gh_package.run.sh
@@ -4,9 +4,14 @@
 # CU HPC Alpine environment. Created to help decrease time related
 # to testing image run and hardware resource requests (where possible).
 
+export TARGET_PLATFORM=linux/amd64
+
 # build the docker image as a singularity image
 docker run -it --platform $TARGET_PLATFORM \
-    --volume $PWD/image:/image \
+    --volume $PWD:/rtx-kg2 \
     --workdir /image \
     --privileged \
-    quay.io/singularity/singularity:v4.0.1
+    quay.io/singularity/singularity:v4.0.1 \
+    shell \
+    --bind /rtx-kg2/cudbmi-set/kg2-build:/home/ubuntu/kg2-build \
+    /rtx-kg2/image/kg2-cudbmi-set_latest.sif
