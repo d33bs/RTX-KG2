c99f7941378e28f512cd787a58d824d6b13878bc
d33bs <dave.bunten@cuanschutz.edu>
2023-12-08 10:41:56 -0700

    create a lima simulation for apptainer

---
+++ cudbmi-set/simulate.hpc.alpine.singularity.run.lima.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Shell script to simulate the run of extended rtx-kg2 image on
+# CU HPC Alpine environment. Created to help decrease time related
+# to testing image run and hardware resource requests (where possible).
+
+# presumes an existing installation of lima
+limactl start --vm-type=qemu --arch=x86_64 template://ubuntu-lts
+
+# enter the shell of the ubuntu-lts instance
+limactl shell ubuntu-lts
+
+# following instructions under https://apptainer.org/docs/admin/main/installation.html
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:apptainer/ppa
+sudo apt update
+sudo apt install -y apptainer
+
+# test the installation
+apptainer exec docker://alpine cat /etc/alpine-release
+
+# show the apptainer build configuration
+apptainer buildcfg
